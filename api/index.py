c768ef808f0833bf91309432ce05dabaaae5eec6
nguyentrinhquy1411 <nguyentrinhquy1411@gmail.com>
2026-03-20 08:53:13 +0700

    deploy

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-from house_price_prediction.webapp.main import app
-
-# Ensure FastAPI object is exposed as 'app'
-
--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-from src.house_price_prediction.webapp.main import app
+import sys
+import os
+
+# Root directory pathing for the python builder
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+from house_price_prediction.webapp.main import app
